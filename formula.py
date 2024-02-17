921ef29f6a7c4796c83d3a81a617cc7d67c5d810
Moleus <fafufuburr@gmail.com>
2024-02-17 11:29:03 +0300

    feat: correct formula for 4 bit comparator

---
+++ formula.py
@@ -0,0 +1,15 @@
+# wolfram formula template
+x0 = "(NOT ( a0 XOR b0 ))" # a0 == b0
+x1 = "(NOT ( a1 XOR b1 ))" # a1 == b1
+x2 = "(NOT ( a2 XOR b2 ))" # a2 == b2
+x3 = "(NOT ( a3 XOR b3 ))" # a3 == b3
+
+# (a1 NAND NOT b1) == a1 > b1
+
+a_greater_b_2bit = f"(a1 NAND NOT b1) NAND ( {x1} NAND ( a0 AND NOT b0 ) ) table"
+a_greater_b_3_bit = f"(a2 NAND NOT b2) NAND ( {x2} NAND ((a1 NAND NOT b1) NAND ({x1} NAND ( a0 AND NOT b0 )))) table"
+a_greater_b_4_bit = f"(a3 NAND NOT b3) NAND ( {x3} NAND ((a2 NAND NOT b2) NAND ({x2} NAND ((a1 NAND NOT b1) NAND ({x1} NAND ( a0 AND NOT b0 )))))) table"
+
+# (a3 NAND NOT b3) NAND ( (NOT ( a3 XOR b3 )) NAND ((a2 NAND NOT b2) NAND ((NOT ( a2 XOR b2 )) NAND ((a1 NAND NOT b1) NAND ((NOT ( a1 XOR b1 )) NAND ( a0 AND NOT b0 )))))) table
+# https://www.wolframalpha.com/input?i=%28a3+NAND+NOT+b3%29+NAND+%28+%28NOT+%28+a3+XOR+b3+%29%29+NAND+%28%28a2+NAND+NOT+b2%29+NAND+%28%28NOT+%28+a2+XOR+b2+%29%29+NAND+%28%28a1+NAND+NOT+b1%29+NAND+%28%28NOT+%28+a1+XOR+b1+%29%29+NAND+%28+a0+AND+NOT+b0+%29%29%29%29%29%29+table
+print(a_greater_b_4_bit)
